092b5687fa4c0336482082a18bd2a874e9162e31
Alex Verevkine <alex.v@cream-software.com>
2018-12-29 23:23:37 +0200

    fix: test dependencies and server tests.

--- server/tests/routes/videos.test.js
+++ server/tests/routes/videos.test.js
@@ -1,17 +1,15 @@
 const app = require("../../app");
 const chai = require("chai");
-const chaiHttp = require("chai-http");
+const request = require("supertest");
 const expect = chai.expect;
 const HttpStatus = require("http-status-codes");
-chai.use(chaiHttp);
 
 describe("GET /api/videos", function() {
   it("should GET videos array", function(done) {
-    chai
-      .request(app)
+    request(app)
       .get("/api/videos")
       .end(function(err, res) {
-        expect(res).to.have.status(HttpStatus.OK);
+        expect(res.status).to.eq(HttpStatus.OK);
         expect(res.body.data).to.be.an("array");
         done();
       });
@@ -20,11 +18,10 @@ describe("GET /api/videos", function() {
 
 describe("GET to not existing path", function() {
   it("should response with 404", function(done) {
-    chai
-      .request(app)
+    request(app)
       .get("/api/notexisting")
       .end(function(err, res) {
-        expect(res).to.have.status(HttpStatus.NOT_FOUND);
+        expect(res.status).to.eq(HttpStatus.NOT_FOUND);
         done();
       });
   });
